98dbc6ed452bf2d5789cf5a6aa9cb329445ffe74
Fabrice Semti <fabrice@fabricesemti.com>
2024-03-15 09:24:00 +0000

    feat: 🚀 use tasks to install pre-requisites

--- install_ansible.sh
+++ install_ansible.sh
@@ -9,13 +9,13 @@ sudo apt install -y aptitude ansible dialog
 sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq &&\
     sudo chmod +x /usr/bin/yq    
 
-# Install used collections    
-ansible-galaxy collection install bec.shell --force
+# # Install used collections    
+# ansible-galaxy collection install bec.shell --force
 
-# Install used roles
-ansible-galaxy role install danielrolls.nix  --force
-ansible-galaxy role install dotstrap.fzf  --force
-ansible-galaxy role install gantsign.oh-my-zsh  --force
-ansible-galaxy role install geerlingguy.docker --force
-ansible-galaxy role install geerlingguy.pip --force
+# # Install used roles
+# ansible-galaxy role install danielrolls.nix  --force
+# ansible-galaxy role install dotstrap.fzf  --force
+# ansible-galaxy role install gantsign.oh-my-zsh  --force
+# ansible-galaxy role install geerlingguy.docker --force
+# ansible-galaxy role install geerlingguy.pip --force
 
