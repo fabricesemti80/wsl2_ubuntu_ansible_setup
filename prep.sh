1168e9d9de44bca6ce588d077cb69d961ebead05
Fabrice Semti <fabrice@fabricesemti.com>
2024-04-27 16:35:07 +0100

    feat: ⚡️ better installer script

---
+++ prep.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# add Ansible repo
+sudo apt-add-repository ppa:ansible/ansible -y
+
+# Install pre-requisites
+sudo apt update
+sudo apt install -y aptitude ansible dialog
+sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq &&\
+    sudo chmod +x /usr/bin/yq    
+
+# # Install used collections    
+# ansible-galaxy collection install bec.shell --force
+
+# # # Install used roles
+# ansible-galaxy role install danielrolls.nix  --force
+# ansible-galaxy role install dotstrap.fzf  --force
+
+# ansible-galaxy role install gantsign.antigen  --force
+# ansible-galaxy role install gantsign.minikube  --force
+# ansible-galaxy role install gantsign.oh-my-zsh  --force
+
+# ansible-galaxy role install geerlingguy.docker --force
+# ansible-galaxy role install geerlingguy.pip --force
+
+# Install Go Tasks
+sudo apt-get install build-essential -y
+sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d
